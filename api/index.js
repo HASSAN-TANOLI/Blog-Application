6352bf45d8bf119d0400dbbf127793592d1d5d7d
Hassan-Tanoli22 <hassantanoli309@gmail.com>
2022-03-02 01:07:10 +0500

    last commit of the day

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const authRoute = require('./routes/auth');
+const usersRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 
 dotenv.config();
 app.use(express.json());
@@ -15,6 +17,8 @@ app.use(express.json());
  }); 
 
  app.use("/api/auth", authRoute);
+ app.use("/api/users", usersRoute);
+  app.use("/api/posts", postRoute);
 
 
 
